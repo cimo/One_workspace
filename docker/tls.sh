6fd0e43dc56a2ff66fc05b93f2a52031afc1076e
simone.dagostino <simone.dagostino@jp.kpmg.com>
2023-04-20 10:20:38 +0900

    update

--- docker/tls.sh
+++ docker/tls.sh
@@ -4,4 +4,4 @@ openssl req -x509 -newkey rsa:4096 -sha256 -days 365 -nodes \
     -keyout /home/root/certificate/tls.key \
     -out /home/root/certificate/tls.crt -subj "/C=JP/ST=Tokyo/L=Tokyo/O=CIMO/OU=CIMO/CN=$DOMAIN" > /var/log/one_workspace/tls.log 2>&1 \
 
-chmod 0644 /home/root/certificate/tls.key \
+chmod 0644 /home/root/certificate/tls.key
