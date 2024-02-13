f93f50d54a8f5631bf88b00a7c018a5686723b87
simone.dagostino <simone.dagostino@jp.kpmg.com>
2024-02-14 00:10:37 +0900

    update

--- README.md
+++ README.md
@@ -2,21 +2,17 @@
 
 Docker service container for have, in one space, a complete develop tools that looks and work in same way on all machine.
 
-Rename "/env/local.env.public" in "/env/local.env" and adjust the variable for your environment.
-
 ## Info:
 
 -   Cross platform (Windows, Linux)
 -   X11 for WSL2 (Run linux GUI app directly in windows)
-
-## Conatiner:
-
 -   NodeJs 18.16.0
 
-## Setup WSL
+## Installation
 
 1. Wrinte on terminal:
 
 ```
-docker compose -f docker-compose.yaml --env-file ./env/local.env up --detach --build --pull "always"
+docker compose -f docker-compose.yaml --env-file ./env/local.env build --no-cache \
+&& docker compose -f docker-compose.yaml --env-file ./env/local.env up --detach --pull "always"
 ```
--- docker/tls.sh
+++ docker/tls.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
-openssl req -x509 -newkey rsa:4096 -sha256 -days 365 -nodes \
-    -keyout /home/root/certificate/tls.key \
-    -out /home/root/certificate/tls.crt -subj "/C=JP/ST=Tokyo/L=Tokyo/O=CIMO/OU=CIMO/CN=$DOMAIN" > /var/log/one_workspace/tls.log 2>&1 \
+PATH_CRT="/home/root/certificate/tls.crt"
+PATH_KEY="/home/root/certificate/tls.key"
+PATH_LOG="/home/root/log/tsl.log"
 
-chmod 0644 /home/root/certificate/tls.key
+generate() {
+    openssl req -x509 -newkey rsa:4096 -sha256 -days 365 -nodes \
+        -keyout "$PATH_KEY" \
+        -out "$PATH_CRT" \
+        -addext "subjectAltName=DNS:localhost" \
+        -subj "/C=JP/ST=Tokyo/L=Tokyo/O=CIMO/OU=ONE-WORKSPACE/CN=$DOMAIN" >> "$PATH_LOG" 2>&1
+
+    chmod 0644 "$PATH_KEY"
+
+    echo "New certificate generated." >> "$PATH_LOG"
+}
+
+if [ -f "$PATH_CRT" ];
+then
+    expiry=$(openssl x509 -enddate -noout -in "$PATH_CRT" | cut -d= -f2)
+    expiryTimestamp=$(date -d "$expiry" +%s)
+    currentDateTimestamp=$(date +%s)
+    expiryDifference=$((expiryTimestamp - currentDateTimestamp))
+
+    if [ "$expiryDifference" -lt 259200 ];
+    then
+        echo "Current certificate expires within 3 days... Generating a new certificate." >> "$PATH_LOG"
+
+        generate
+    fi
+else
+    echo "Certificate does not exist... Generating a new certificate." >> "$PATH_LOG"
+
+    generate
+fi
