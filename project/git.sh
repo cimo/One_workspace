77f4992b248937bc23d8ce1348dd75832364cbb0
cimo <simone.dagostino@gmail.com>
2025-02-24 15:40:57 +0900

    update

--- project/git.sh
+++ project/git.sh
@@ -2,8 +2,10 @@
 
 git config --global credential.helper cache
 
+PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # 1
-cd project/authentication/root/
+cd "${PROJECT_ROOT}/authentication/root/"
 
 echo "- Execute git on AUTHENTICATION"
 
@@ -12,7 +14,7 @@ git fetch --all
 git pull
 
 # 2
-cd ../../form-data_parser/root/
+cd "${PROJECT_ROOT}/form-data_parser/root/"
 
 echo "- Execute git on FORM-DATA_PARSER"
 
@@ -21,7 +23,7 @@ git fetch --all
 git pull
 
 # 3
-cd ../../jidou/root/
+cd "${PROJECT_ROOT}/jidou/root/"
 
 echo "- Execute git on JIDOU"
 
@@ -30,7 +32,7 @@ git fetch --all
 git pull
 
 # 4
-cd ../../jsmvcfw/root/
+cd "${PROJECT_ROOT}/jsmvcfw/root/"
 
 echo "- Execute git on JSMVCFW"
 
@@ -39,7 +41,7 @@ git fetch --all
 git pull
 
 # 5
-cd ../../kaori_ai/root/
+cd "${PROJECT_ROOT}/kaori_ai/root/"
 
 echo "- Execute git on KAORI_AI"
 
@@ -48,7 +50,7 @@ git fetch --all
 git pull
 
 # 6
-cd ../../pid/root/
+cd "${PROJECT_ROOT}/pid/root/"
 
 echo "- Execute git on PID"
 
@@ -57,7 +59,7 @@ git fetch --all
 git pull
 
 # 7
-cd ../../request/root/
+cd "${PROJECT_ROOT}/request/root/"
 
 echo "- Execute git on REQUEST"
 
@@ -66,7 +68,7 @@ git fetch --all
 git pull
 
 # 8
-cd ../../websocket/root/
+cd "${PROJECT_ROOT}/websocket/root/"
 
 echo "- Execute git on WEBSOCKET"
 
