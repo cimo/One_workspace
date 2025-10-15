2b7be13affde0e36b4fb72b2078c8c72da82c17e
cimo <simone.dagostino@gmail.com>
2025-10-15 22:26:38 +0900

    update

--- project/git.sh
+++ project/git.sh
@@ -4,13 +4,14 @@ git config --global credential.helper cache
 
 PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-for directory in "${PROJECT_ROOT}"/*/; do
+for directory in ${PROJECT_ROOT}/*/; do
   name=$(basename "${directory}")
 
   echo -e "\n- Execute git on ${name^^}"
 
   cd "${name}" || continue
 
+  git config --global --add safe.directory ${directory}
   git checkout main
   git fetch --all
   git pull
