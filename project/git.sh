53cbc62810fec6bee16f8e5702745b40eb68ccd9
cimo <simone.dagostino@gmail.com>
2025-10-22 19:12:51 +0900

    update

--- project/git.sh
+++ project/git.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-git config --global credential.helper cache
-
 PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+USERNAME=$(stat -c '%U' "${PROJECT_ROOT}")
+
+sudo -u ${USERNAME} git config --global credential.helper cache
+sudo -u ${USERNAME} git config --global --unset-all safe.directory
+
 for directory in ${PROJECT_ROOT}/*/; do
-  name=$(basename "${directory}")
+  if [ -d "${directory}/.git" ]; then
+    name=$(basename "${directory}")
 
-  echo -e "\n- Execute git on ${name^^}"
+    sudo -u ${USERNAME} git config --global --add safe.directory ${directory}
 
-  cd "${name}" || continue
+    echo -e "\n- Execute git on ${name^^}"
 
-  git config --global --add safe.directory ${directory}
-  git checkout main
-  git fetch --all
-  git pull
+    cd "${directory}" || continue
 
-  cd "${PROJECT_ROOT}"
+    if sudo -u ${USERNAME} git rev-parse --verify main >/dev/null 2>&1; then
+      sudo -u ${USERNAME} git checkout main
+      sudo -u ${USERNAME} git fetch --all
+      sudo -u ${USERNAME} git pull
+    else
+      echo "Repository 'main' not found."
+    fi
+
+    cd "${PROJECT_ROOT}"
+  fi
 done
