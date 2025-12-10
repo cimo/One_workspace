8da5711ca3b9e368abe96085d6e5d3455a9d6dc8
cimo <simone.dagostino@gmail.com>
2025-12-11 02:23:49 +0900

    update

--- script/tauri_windows_sign.sh
+++ script/tauri_windows_sign.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-pathCrt="${PATH_ROOT}project/${1}/src-tauri/target/certificate/tauri_sign.crt"
-pathKey="${PATH_ROOT}project/${1}/src-tauri/target/certificate/tauri_sign.key"
-pathPfx="${PATH_ROOT}project/${1}/src-tauri/target/certificate/tauri_sign.pfx"
+projectName=$(basename "$(echo "$(pwd)" | sed 's|/src-tauri.*||')")
+pathRelease=$(ls -d "${PATH_ROOT}project/${projectName}/src-tauri/target/"*/release/ | sort | tail -1)
+echo "cimo1 ${projectName}"
+echo "cimo2 ${pathRelease}"
+pathCrt="${PATH_ROOT}project/${projectName}/src-tauri/target/certificate/tauri_sign.crt"
+pathKey="${PATH_ROOT}project/${projectName}/src-tauri/target/certificate/tauri_sign.key"
+pathPem="${PATH_ROOT}project/${projectName}/src-tauri/target/certificate/tauri_sign.pem"
+pathPfx="${PATH_ROOT}project/${projectName}/src-tauri/target/certificate/tauri_sign.pfx"
 
-input="${PATH_ROOT}project/${1}/src-tauri/target/x86_64-pc-windows-msvc/release/${1}.exe"
-output="${PATH_ROOT}project/${1}/src-tauri/target/x86_64-pc-windows-msvc/release/${1}_signed.exe"
+input="${pathRelease}${projectName}.exe"
+output="${PATH_ROOT}project/${projectName}/src-tauri/target/release/${projectName}_signed.exe"
 
 passout=$(openssl rand -base64 16)
 
-rm -r "${PATH_ROOT}project/${1}/src-tauri/target/certificate/"
-mkdir -p "${PATH_ROOT}project/${1}/src-tauri/target/certificate/"
-rm "${output}"
+rm -r "${PATH_ROOT}project/${projectName}/src-tauri/target/certificate/"
+mkdir "${PATH_ROOT}project/${projectName}/src-tauri/target/certificate/"
+rm -f "${output}"
 
 openssl req -x509 -newkey rsa:4096 -sha256 -days 365 -nodes \
         -keyout "${pathKey}" \
@@ -20,21 +25,23 @@ openssl req -x509 -newkey rsa:4096 -sha256 -days 365 -nodes \
         -addext "keyUsage=digitalSignature" \
         -subj "/C=JP/ST=Tokyo/L=Tokyo/O=CIMO/OU=ONE-WORKSPACE/CN=${DOMAIN}"
 
+chmod 0644 "${pathKey}"
+
+cat "${pathCrt}" "${pathKey}" > "${pathPem}"
+
 openssl pkcs12 -export \
   -inkey "${pathKey}" \
   -in "${pathCrt}" \
-  -name "Tauri signing" \
+  -name "tauriSigning" \
   -out "${pathPfx}" \
   -passout pass:"${passout}"
 
 osslsigncode sign \
   -pkcs12 "${pathPfx}" \
   -pass "${passout}" \
-  -n "${1}" \
+  -n "${projectName}" \
   -i "${TAURI_APP_URL}" \
   -h sha256 \
   -ts "http://timestamp.sectigo.com" \
   -in "${input}" \
   -out "${output}"
-
-#osslsigncode verify -in "${PATH_ROOT}project/${1}/src-tauri/target/x86_64-pc-windows-msvc/release/${1}_signed.exe"
