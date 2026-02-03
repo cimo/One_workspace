9a221bcc0473e64c6865a02b44dd741bdbe9c959
simone.dagostino <simone.dagostino@jp.kpmg.com>
2026-02-03 09:35:38 +0900

    update

--- script/dbus.sh
+++ script/dbus.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export XDG_RUNTIME_DIR="/run/user/$(id -u)"
-export DBUS_SESSION_BUS_ADDRESS="unix:path=${XDG_RUNTIME_DIR}/bus"
-
-dbus-daemon --session --address="${DBUS_SESSION_BUS_ADDRESS}" --fork
+XDG_RUNTIME_DIR="/run/user/$(id -u)" \
+DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus" \
+dbus-daemon --session --address="unix:path=/run/user/$(id -u)/bus" --fork
