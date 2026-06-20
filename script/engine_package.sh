0e54bee53f0e9f910b0b5675893e3a420cfc2107
cimo <simone.dagostino@gmail.com>
2026-06-20 20:43:54 +0900

    update

--- script/engine_package.sh
+++ script/engine_package.sh
@@ -18,7 +18,7 @@ fi
 
 if [ ${isGpu} = true ]
 then
-    python3 -m pip install torch==2.10.0 torchvision==0.25.0 torchaudio==2.10.0 --index-url https://download.pytorch.org/whl/cu130
+    python3 -m pip install torch==2.10.0 torchvision==0.25.0 torchaudio==2.10.0 --index-url https://download.pytorch.org/whl/cu120
 else
     python3 -m pip install torch==2.10.0 torchvision==0.25.0 torchaudio==2.10.0 --index-url https://download.pytorch.org/whl/cpu
 fi
