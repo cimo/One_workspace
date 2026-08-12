d4ddb57b5cf9c7be4863f1235b3e6e95480a92c1
simone.dagostino <simone.dagostino@jp.kpmg.com>
2026-08-12 15:41:05 +0900

    update

--- script/dbus.sh
+++ script/dbus.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-xdgRuntimeDir="${XDG_RUNTIME_DIR}"
-userId="/run/user/$(id -u)/"
+xdgRuntimeDirectory="${XDG_RUNTIME_DIR}"
+userRuntimeDirectory="/run/user/$(id -u)/"
 
-XDG_RUNTIME_DIR="${userId}" \
-DBUS_SESSION_BUS_ADDRESS="unix:path=${userId}bus" \
-dbus-daemon --session --address="unix:path=${userId}bus" --fork
+XDG_RUNTIME_DIR="${userRuntimeDirectory}" \
+DBUS_SESSION_BUS_ADDRESS="unix:path=${userRuntimeDirectory}/bus" \
+dbus-daemon --session --address="unix:path=${userRuntimeDirectory}/bus" --fork
 
-ln -sf "${userId}bus" "${xdgRuntimeDir}bus"
+ln -sf "${userRuntimeDirectory}/bus" "${xdgRuntimeDirectory}/bus"
